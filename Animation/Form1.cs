3655742653e6304406b3b66b7fa2e3aaa33c5c39
Bùi Minh Tuấn <92067348+tuan20520342@users.noreply.github.com>
2021-10-30 14:56:19 +0700

    Add project files.

---
+++ Animation/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animation
+{
+    public partial class Form1 : Form
+    {
+        int n = 0;
+        Point[] a = new Point[100];
+        SolidBrush[] abrush = new SolidBrush[100];
+        Graphics g;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+        
+        private Color RandomColor()
+        {
+            timer1.Start();
+            Random random = new Random();
+            int color = random.Next(1, 7);
+            switch(color)
+            {
+                case 1:
+                    return Color.Red;
+                case 2:
+                    return Color.Orange;
+                case 3:
+                    return Color.Yellow;
+                case 4:
+                    return Color.Green;
+                case 5:
+                    return Color.Blue;
+                case 6:
+                    return Color.Indigo;
+                case 7:
+                    return Color.Violet;
+            }
+            return Color.White;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            a[n].X = e.X;
+            a[n].Y = e.Y;
+            SolidBrush mybrush = new SolidBrush(RandomColor());
+            abrush[n] = mybrush;
+            g = this.CreateGraphics();
+            Rectangle shape = new Rectangle(a[n].X, a[n].Y, 50, 50);
+            g.FillEllipse(mybrush, shape);
+            n = n + 1;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                a[i].Y = a[i].Y + 5;
+                Rectangle shape = new Rectangle(a[i].X, a[i].Y, 50, 50);
+                g.FillEllipse(abrush[i], shape);
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
